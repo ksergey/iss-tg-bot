6f4a150d6530823d393664ff8c9c62ef258cbe02
Sergey Kovalevich <inndie@gmail.com>
2024-11-18 23:05:13 +0300

    update

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -5,23 +5,26 @@ from aiogram.types import Message, BotCommand
 from aiogram.filters import Command
 
 commands = [
-    BotCommand(command='vwap', description='calculate vwap for a security'),
-    BotCommand(command='help', description='show help')
+    BotCommand(command='vwap', description='Посчитать VWAP цену'),
+    BotCommand(command='vwapt', description='Посчитать время окончания работы алгоритма VWAP'),
+    BotCommand(command='help', description='Помощь по командам')
 ]
 
 router = Router()
 
 @router.message(Command('help'))
 async def handler_command_help(message: Message):
-    help_message = 'commands:\n'
+    help_message = 'Доступные команды:\n'
     help_message += ''.join(
         f'/{command.command} - {command.description}\n' for command in commands
     )
     help_message += '\n'
-    help_message += 'usage examples:\n'
-    help_message += '<i>/vwap LKOH</i> - vwap for current day\n'
-    help_message += '<i>/vwap LKOH 12:00 16:00</i> - calc vwap for current day from 12:00 to 16:00\n'
-    help_message += '<i>/vwap LKOH 13:00</i> - calc vwap for current day from 13:00\n'
+    help_message += 'Примеры использования:\n'
+    help_message += '<i>/vwap LKOH</i> - Посчитать VWAP цену для инструмента LKOH с начала дня и до текущего момента\n'
+    help_message += '<i>/vwap LKOH 12:00 16:00</i> - Посчитать VWAP цену для инструмента LKOH с 12:00 до 16:00 текущего дня\n'
+    help_message += '<i>/vwap LKOH 13:00</i> - Посчитать VWAP цену для инструмента LKOH с 13:00 и до текущего момента\n'
+    help_message += '<i>/vwapt YDEX 13:00 1000</i> - Посчитать время окончания работы VWAP алгоритма для инструмента YDEX с 13:00, объем 1000 лотов, процент объема 100%\n'
+    help_message += '<i>/vwapt YDEX 13:00 1000 30</i> - Посчитать время окончания работы VWAP алгоритма для инструмента YDEX с 13:00, объем 1000 лотов, процент объема 30%\n'
 
     await message.answer(help_message)
 
--- app/handlers/iss.py
+++ app/handlers/iss.py
@@ -21,13 +21,45 @@ async def calc_vwap(iss: MoexISS, args: str):
 
     df = await iss.get_trades(symbol)
     if df.empty:
-        raise Exception('no data')
+        raise Exception('Нет данных')
     if begin:
         df = df.loc[df['TRADETIME'] >= begin]
     if end:
         df = df.loc[df['TRADETIME'] < end]
     if df.empty:
-        raise Exception('no data')
+        raise Exception('Нет данных')
+
+    vwap = np.average(df['PRICE'], weights=df['QUANTITY'])
+    total_qty = np.sum(df['QUANTITY'])
+    first_trade_time = df.iloc[0]['TRADETIME']
+    last_trade_time = df.iloc[-1]['TRADETIME']
+
+    return (symbol, vwap, total_qty, first_trade_time, last_trade_time)
+
+async def calc_vwap_time(iss: MoexISS, args: str):
+    symbol, begin, target_qty, percent, *_ = args.split(' ') + [ None, None, None ]
+    symbol = symbol.upper()
+
+    if not begin:
+        raise Exception('Точка отсчета не задана')
+
+    if not target_qty:
+        raise Exception('Объем не задан')
+    target_qty = float(target_qty)
+
+    if not percent:
+        percent = 100.0
+    percent = float(percent)
+    if percent < 0.0 or percent > 100.0:
+        raise Exception('Процент объема должен быть больше 0 и меньше либо равен 100')
+
+    df = await iss.get_trades(symbol)
+    if df.empty:
+        raise Exception('Нет данных')
+    df = df.loc[df['TRADETIME'] >= begin]
+    df = df.loc[df['QUANTITY'].cumsum() * (percent / 100.0) <= target_qty] # TODO: тут что-то не так (не хватает 1й сделки)
+    if df.empty:
+        raise Exception('Нет данных')
 
     vwap = np.average(df['PRICE'], weights=df['QUANTITY'])
     total_qty = np.sum(df['QUANTITY'])
@@ -39,19 +71,25 @@ async def calc_vwap(iss: MoexISS, args: str):
 class VWAPUpdate(CallbackData, prefix='vwap', sep=';'):
     args: str
 
-def make_reply_keyboard(args: str) -> InlineKeyboardMarkup:
+class VWAPTimeUpdate(CallbackData, prefix='vwap-time', sep=';'):
+    args: str
+
+def make_reply_keyboard(args: str, CallbackType) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(
-        text=f'\N{Clockwise Open Circle Arrow} Update',
-        callback_data=VWAPUpdate(args=args)
+        text=f'\N{Clockwise Open Circle Arrow} Обновить',
+        callback_data=CallbackType(args=args)
     )
     return builder.as_markup(resize_keyboard=True)
 
 def make_vwap_message(symbol, vwap, total_qty, first_trade_time, last_trade_time) -> str:
     return f'<b>{symbol}</b> {vwap:.2f}@{total_qty} ({first_trade_time} - {last_trade_time})'
 
+def make_vwap_time_message(symbol, vwap, total_qty, first_trade_time, last_trade_time) -> str:
+    return f'<b>{symbol}</b> {vwap:.2f}@{total_qty} ({first_trade_time} - {last_trade_time})'
+
 @router.callback_query(VWAPUpdate.filter())
-async def callback_update(query: CallbackQuery, callback_data: VWAPUpdate, iss: MoexISS):
+async def callback_vwap_update(query: CallbackQuery, callback_data: VWAPUpdate, iss: MoexISS):
     edit_message = query.message
     if not edit_message:
         return
@@ -66,11 +104,36 @@ async def callback_update(query: CallbackQuery, callback_data: VWAPUpdate, iss: 
         if edit_message.html_text != text:
             await edit_message.edit_text(
                 text,
-                reply_markup=make_reply_keyboard(callback_data.args)
+                reply_markup=make_reply_keyboard(callback_data.args, VWAPUpdate)
             )
-        await query.answer('done')
+        await query.answer('Готово')
     except Exception as ex:
-        await query.answer(f'\N{Heavy Ballot X} something goes wrong')
+        await query.answer(f'\N{Heavy Ballot X} Что-то пошло не так')
+        logger.exception(f'exception during process message {message}')
+    finally:
+        await notification_message.delete()
+
+@router.callback_query(VWAPTimeUpdate.filter())
+async def callback_vwap_time_update(query: CallbackQuery, callback_data: VWAPTimeUpdate, iss: MoexISS):
+    edit_message = query.message
+    if not edit_message:
+        return
+    message = edit_message.reply_to_message
+    if not message:
+        return
+
+    notification_message = await message.answer('\N{Sleeping Symbol}...')
+    try:
+        (symbol, vwap, total_qty, first_trade_time, last_trade_time) = await calc_vwap_time(iss, callback_data.args)
+        text = make_vwap_time_message(symbol, vwap, total_qty, first_trade_time, last_trade_time)
+        if edit_message.html_text != text:
+            await edit_message.edit_text(
+                text,
+                reply_markup=make_reply_keyboard(callback_data.args, VWAPTimeUpdate)
+            )
+        await query.answer('Готово')
+    except Exception as ex:
+        await query.answer(f'\N{Heavy Ballot X} Что-то пошло не так')
         logger.exception(f'exception during process message {message}')
     finally:
         await notification_message.delete()
@@ -81,9 +144,22 @@ async def handler_command_vwap(message: Message, command: CommandObject, iss: Mo
     try:
         (symbol, vwap, total_qty, first_trade_time, last_trade_time) = await calc_vwap(iss, command.args)
         text = make_vwap_message(symbol, vwap, total_qty, first_trade_time, last_trade_time)
-        await message.reply(text, reply_markup=make_reply_keyboard(command.args))
+        await message.reply(text, reply_markup=make_reply_keyboard(command.args, VWAPUpdate))
     except Exception as ex:
-        await message.reply(f'\N{Heavy Ballot X} something goes wrong')
+        await message.answer(f'\N{Heavy Ballot X} Что-то пошло не так')
+        logger.exception(f'exception during process message {message}')
+    finally:
+        await notification_message.delete()
+
+@router.message(Command('vwapt'))
+async def handler_command_vwapt(message: Message, command: CommandObject, iss: MoexISS):
+    notification_message = await message.answer('\N{Sleeping Symbol}...')
+    try:
+        (symbol, vwap, total_qty, first_trade_time, last_trade_time) = await calc_vwap_time(iss, command.args)
+        text = make_vwap_time_message(symbol, vwap, total_qty, first_trade_time, last_trade_time)
+        await message.reply(text, reply_markup=make_reply_keyboard(command.args, VWAPTimeUpdate))
+    except Exception as ex:
+        await message.answer(f'\N{Heavy Ballot X} Что-то пошло не так')
         logger.exception(f'exception during process message {message}')
     finally:
         await notification_message.delete()
@@ -93,9 +169,9 @@ async def handler_command_vwap_reset(message: Message, iss: MoexISS):
     notification_message = await message.answer('\N{Sleeping Symbol}...')
     try:
         iss.reset()
-        await message.reply('done')
+        await message.reply('Готово')
     except Exception as ex:
-        await message.reply(f'\N{Heavy Ballot X} error: {ex}')
+        await message.reply(f'\N{Heavy Ballot X} Ошибка: {ex}')
         logger.exception(f'exception during process message {message}')
     finally:
         await notification_message.delete()
